c9fd7d6166218298379ee9cb02808e078b20351a
JonYeong <Jonathan.yeong@yahoo.com>
2026-06-13 17:23:39 +0800

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        idx = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in idx:
+                return [min(idx[complement], i), max(idx[complement], i)]
+            idx[nums[i]] = i
